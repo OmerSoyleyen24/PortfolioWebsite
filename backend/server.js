cd8101ac17706bcfcb6b346267f3da521f97dad7
Ömer Söyleyen <153733533+OmerSoyleyen24@users.noreply.github.com>
2024-10-14 00:32:41 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ app.post('/send-email', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
